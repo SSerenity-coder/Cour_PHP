780ba7a2ea4ee9cabd858f6d5804fe1e7af12d26
KETOH Neal <ketohc1963@gmail.com>
2020-05-21 21:39:51 +0000

    login route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function () {
-    return view('test');
+Route::get('/login', function () {
+    return view('auth.login');
 });
